bec44233e9778db311215ba82f2154163d10fa44
roristada <roristada@gmail.com>
2024-10-08 01:37:19 +0700

    fix uploadv3

--- app/api/user/route.tsx
+++ app/api/user/route.tsx
@@ -6,6 +6,7 @@ const prisma = new PrismaClient();
 export async function GET(request: NextRequest) {
     try {
         const users = await prisma.user.findMany();
+        console.log(process.env.GOOGLE_CLOUD_PROJECT_ID , process.env.GOOGLE_CLOUD_CLIENT_EMAIL , process.env.GOOGLE_CLOUD_PRIVATE_KEY)
         return NextResponse.json(users);
     } catch (error) {
         console.error("Error fetching users:", error);

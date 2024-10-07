a6418d01ba058257eaf1e57be3257464587e2f1c
roristada <roristada@gmail.com>
2024-10-07 21:51:52 +0700

    railway1

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['via.placeholder.com']},
-    reactStrictMode: true,
-    // Add this section
-    server: {
-        port: process.env.PORT || 3000,
-        host: '0.0.0.0'
-    }
-}
+    domains: ['via.placeholder.com'],
+  },
+  reactStrictMode: true,
+  // Add this section
+  server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
+  },
+};
 
-module.exports = nextConfig
+export default nextConfig;
+

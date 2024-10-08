e5e79704be115c43bbdece17290ae707e685716f
roristada <roristada@gmail.com>
2024-10-08 12:06:47 +0700

    fix module3

--- app/api/support/messages/route.tsx
+++ app/api/support/messages/route.tsx
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import { pusher } from "@/lib/pusher";
+import { pusher } from "../../../../lib/pusher";
 
 const prisma = new PrismaClient();
 
